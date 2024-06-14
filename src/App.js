d246fea518adc7a49bde095af3c4628a64f1912f
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-14 16:06:51 -0300

    novas atualizaçoes css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* Inicio Jogos */}
+
       <h2>Secret Word</h2>
 
-      {/* Fim Jogos */}
+
     </div>
   );
-}
+};
 
 export default App;
