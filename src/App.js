c337f4cd055b5f44adc3dfe41d291e5c0654a332
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-15 16:48:31 -0300

    Atualizando e adicionando paginas estilizando css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import StartScreen from './components/StartScreen';
 
 function App() {
   return (
     <div className="App">
 
-      <h2>Secret Word</h2>
+      <StartScreen />
 
 
     </div>
