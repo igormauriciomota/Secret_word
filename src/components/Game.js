68c6ea05648201b36cb6949340a2354538b8e144
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-06-18 22:26:44 -0300

    Atualizaçoes

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,23 +11,23 @@ const Game = ({
     score,
 }) => {
     return (
-        <div className='game'>
+        <div className="game">
             <p className='point'>
                 <span>Pontuação: {score}</span>
             </p>
             <h1>Adivinhe a palavra:</h1>
-            <h3 className='tip'>
+            <h3 className="tip">
                 Dica sobre a palavra: <span>{pickedCategory}</span>
             </h3>
             <p>Você ainda tem {guesses} tentativa</p>
             <div className="wordContainer">
                 {letters.map((letter, i) => (
                     guessedLetters.includes(letter) ? (
-                        <span kei={i} className='letter'>
-                            {letter}
+                        <span kei={i} className="letter">
+                        {letter}
                         </span>
                 ) : (
-                        <span kei={i} className='blankSquare'>
+                        <span kei={i} className="blankSquare">
 
                         </span>
                 )
